eb6b6b2a1cdeb9352c9e56eb8687bf67f1a10375
bill <b.alexandrou@gmail.com>
2012-01-03 09:51:12 +0200

    adding client code

---
+++ client/src/gr/ntua/vision/monitoring/test/LibInvoke.java
@@ -0,0 +1,78 @@
+package gr.ntua.vision.monitoring.test;
+
+import gr.ntua.vision.monitoring.VisionMonitoring;
+import gr.ntua.vision.monitoring.model.Resource;
+import gr.ntua.vision.monitoring.model.impl.ResourceImpl;
+import it.eng.compliance.xdas.parser.XDasEventType;
+import it.eng.compliance.xdas.parser.XdasOutcomes;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+
+/**
+ * Library invoker.
+ */
+public class LibInvoke
+{
+	/***/
+	static final long		Period	= 20000;
+	/***/
+	static final String[][]	Actions	= {
+			{ "http://10.0.2.111:8080/vismo/LibInvoke", "http://10.0.2.111:8080/vismo/Monitoring", "type=event" }, //
+			{ "http://10.0.2.111:8080/vismo/Monitoring/cluster", "http://10.0.2.111:8080/vismo/Monitoring/cloud", "type=event" }, //
+			{ "http://10.0.2.111:8080/vismo/LibInvoke", null }, //
+			{ "http://147.102.19.45/myvisionhost/ManagerContainer/createContainerReplica",
+			"http://66.135.200.23/myvisionhost/ManagerContainer/replicateContainer", "container=container1" }, //
+			{ "http://147.102.19.45/myvisionhost/ManagerObject/createObjectReplica",
+			"http://66.135.200.23/myvisionhost/ManagerObject/replicateObject", "object=object1" }, //
+									};
+
+
+	/**
+	 * test case entry point.
+	 * 
+	 * @param args
+	 *            ignored.
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		UUID appId = UUID.randomUUID();
+		System.out.println( "Initializing app (id= " + appId + ")" );
+		VisionMonitoring.initialize( "http://10.0.2.111:8080/vismo/Monitoring/push/event", appId );
+
+		while( true )
+		{
+			Thread.sleep( Period );
+
+			List<Resource> resources = Lists.newArrayList();
+
+			resources.add( new ResourceImpl( "memory", "MB", Math.random() * 3096 ) );
+			resources.add( new ResourceImpl( "storage", "GB", Math.random() * 1024 ) );
+
+			String[] action = Actions[Math.min( (int) Math.floor( Actions.length * Math.random() ), Actions.length - 1 )];
+
+			URL src = new URL( action[0] );
+			URL trg = action[1] == null ? null : new URL( action[1] );
+
+			Map<String, String> params = Maps.newHashMap();
+			for( int i = 2; i < action.length; ++i )
+			{
+				String[] parts = action[i].split( "=" );
+				params.put( parts[0], parts[1] );
+			}
+
+			VisionMonitoring.instance().log( src, trg, "foo", "bar",
+												XDasEventType.XDAS_AE_MODIFY_DATA_ITEM_ASSOC_CONTEXT.getEventCode(),
+												XdasOutcomes.XDAS_OUT_SUCCESS.getOutcomeCode(), params, resources );
+
+			System.out.println( "action saved." );
+		}
+	}
+}
---
+++ client/src/gr/ntua/vision/monitoring/test/RulesTest.java
@@ -0,0 +1,119 @@
+package gr.ntua.vision.monitoring.test;
+
+import gr.ntua.vision.monitoring.model.Event;
+import gr.ntua.vision.monitoring.model.impl.EventImpl;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONStringer;
+import org.codehaus.jettison.json.JSONWriter;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
+import com.sun.jersey.api.representation.Form;
+
+
+/**
+ * Rules test program.
+ */
+@Path("/push")
+public class RulesTest
+{
+	/***/
+	static final Client	client			= Client.create();
+
+	static final String	Rule0			= "rule \"test rule name\"\n" + //
+												"when\n" + //
+												"        Event( Source.Address like \".+\" );\n" + //
+												"then\n" + //
+												"         PushAsIs( \"http://10.0.0.34:7070/push\" );\n";
+
+	/***/
+	static int			actions			= 0;
+	/***/
+	static int			measurements	= 0;
+
+
+	/**
+	 * This is the service implementing method.
+	 * 
+	 * @param eventJSON
+	 *            the JSON string which specifies the event.
+	 * @return the operation status.
+	 * @throws JSONException
+	 */
+	@POST
+	@Produces("application/json")
+	public String receiveEvent(@FormParam("event") String eventJSON) throws JSONException
+	{
+		System.out.println( eventJSON );
+
+		// Event event = new EventImpl( new JSONObject( eventJSON ) );
+		// switch( event.eventType() )
+		// {
+		// case Measurement:
+		// ++measurements;
+		// break;
+		// case Action:
+		// ++actions;
+		// break;
+		// }
+		//
+		// System.out.println( "received event (Type=" + ( event.aggregationCount() == 0 ? "single" : "aggregated" ) + " || A:"
+		// + actions + " / M:" + measurements + ")" );
+		// System.out.println( "\tDescription: " + event.toJSON() );
+
+		final JSONWriter wr = new JSONStringer().object();
+		wr.key( "status" ).value( "ok" );
+		return wr.endObject().toString();
+	}
+
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws IllegalArgumentException
+	 */
+	public static void main(String[] args) throws IllegalArgumentException, IOException
+	{
+		// System.out.println(Rule0);
+		// System.out.println();
+		// System.out.println();
+		// System.out.println();
+		// System.out.println();
+		// System.out.println(Rule1);
+
+		final Map<String, String> initParams = new HashMap<String, String>();
+		initParams.put( "com.sun.jersey.config.property.packages", "gr.ntua.vision.monitoring.test" );
+
+		GrizzlyWebContainerFactory.create( "http://localhost:7070/", initParams );
+
+		System.out.println( "Registering rule." );
+
+		try
+		{
+			Form form = new Form();
+			form.add( "rule", Rule0 );
+			String response = client.resource( "http://10.0.2.215:8080/vismo/Monitoring/cloud/registerAggregationRule" )
+					.post( ClientResponse.class, form ).getEntity( String.class );
+			System.out.println( response );
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+			System.exit( 0 );
+		}
+
+		System.out.println( "w8ing 4 events :P" );
+	}
+}
