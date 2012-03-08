c2e83e45a289f53a6a73c8e4123ee0e8d938ea27
vassilis <alexv@mail.ntua.gr>
2012-03-08 12:34:36 +0200

    adding command client tests

---
+++ src/main/java/gr/ntua/vision/monitoring/CommandClient.java
@@ -0,0 +1,104 @@
+package gr.ntua.vision.monitoring;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+/**
+ * This is used to query any running monitoring instance.
+ */
+public class CommandClient {
+    /** the configuration object. */
+    private final Config cnf;
+
+
+    /**
+     * Constructor.
+     * 
+     * @param cnf
+     *            the configuration object.
+     */
+    public CommandClient(final Config cnf) {
+        this.cnf = cnf;
+    }
+
+
+    /**
+     * @return the PID of the monitoring instance.
+     * @throws IOException
+     */
+    public int status() throws IOException {
+        final DatagramSocket sock = new DatagramSocket();
+
+        try {
+            sock.setSoTimeout( cnf.getTimeout() );
+            send( sock, cnf.getStatusCommand(), InetAddress.getLocalHost(), cnf.getPort() );
+
+            final DatagramPacket dgram = receive( sock );
+
+            return Integer.parseInt( new String( dgram.getData(), 0, dgram.getLength() ) );
+        } finally {
+            sock.close();
+        }
+    }
+
+
+    /**
+     * Ask the running monitoring instance to stop.
+     * 
+     * @throws IOException
+     */
+    public void stop() throws IOException {
+        final DatagramSocket sock = new DatagramSocket();
+
+        try {
+            sock.setSoTimeout( cnf.getTimeout() );
+            send( sock, cnf.getKillCommand(), InetAddress.getLocalHost(), cnf.getPort() );
+            receive( sock );
+        } finally {
+            sock.close();
+        }
+    }
+
+
+    /**
+     * Receive a datagram packet from the given socket.
+     * 
+     * @param sock
+     *            the socket.
+     * @return a {@link DatagramPacket}.
+     * @throws IOException
+     */
+    private static DatagramPacket receive(final DatagramSocket sock) throws IOException {
+        final byte[] buf = new byte[64];
+        final DatagramPacket req = new DatagramPacket( buf, buf.length );
+
+        sock.receive( req );
+
+        return req;
+    }
+
+
+    /**
+     * Send as a datagram packet the given content, through the socket.
+     * 
+     * @param sock
+     *            the socket.
+     * @param payload
+     *            the payload to send.
+     * @param addr
+     *            the address to sent to.
+     * @param port
+     *            the port to sent to.
+     * @throws IOException
+     */
+    private static void send(final DatagramSocket sock, final String payload, final InetAddress addr, final int port)
+            throws IOException {
+        final byte[] buf = payload.getBytes();
+        final DatagramPacket req = new DatagramPacket( buf, buf.length, addr, port );
+
+        sock.send( req );
+    }
+}
---
+++ src/main/java/gr/ntua/vision/monitoring/Config.java
@@ -0,0 +1,75 @@
+package gr.ntua.vision.monitoring;
+
+import java.lang.management.ManagementFactory;
+
+
+/**
+ *
+ */
+public class Config {
+    /***/
+    private static final int    COMMAND_SERVER_PORT = 56431;
+    /***/
+    private static final String KILL_COMMAND        = "kill!";
+    /***/
+    private static final int    pid                 = getVMPID();
+    /***/
+    private static final int    RESPONSE_TIMEOUT    = 1000;
+    /***/
+    private static final String STATUS_COMMAND      = "pid?";
+
+
+    /**
+     * @return
+     */
+    public String getKillCommand() {
+        return KILL_COMMAND;
+    }
+
+
+    /**
+     * @return
+     */
+    public int getPID() {
+        return pid;
+    }
+
+
+    /**
+     * @return
+     */
+    public int getPort() {
+        return COMMAND_SERVER_PORT;
+    }
+
+
+    /**
+     * @return
+     */
+    public String getStatusCommand() {
+        return STATUS_COMMAND;
+    }
+
+
+    /**
+     * @return
+     */
+    public int getTimeout() {
+        return RESPONSE_TIMEOUT;
+    }
+
+
+    /**
+     * @return this vm's pid.
+     */
+    private static int getVMPID() {
+        // NOTE: expecting something like '<pid>@<hostname>'
+        final String vmname = ManagementFactory.getRuntimeMXBean().getName();
+        final int atIndex = vmname.indexOf( "@" );
+
+        if( atIndex < 0 )
+            throw new Error( "Cannot get pid: pid N/A for this jvm." );
+
+        return Integer.valueOf( vmname.substring( 0, atIndex ) );
+    }
+}
