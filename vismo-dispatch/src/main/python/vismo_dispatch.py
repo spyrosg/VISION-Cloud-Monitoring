eb7eef398b47d0317ede1d74801509a48f478278
vassilis <alexv@mail.ntua.gr>
2012-07-11 14:31:29 +0300

    cleaning up vismo_dispatch

--- vismo-dispatch/src/main/python/vismo_dispatch.py
+++ vismo-dispatch/src/main/python/vismo_dispatch.py
@@ -4,8 +4,6 @@
 import socket, struct, fcntl, sys
 from os import getpid
 from time import time
-import logging
-#from logging.handlers import TimedRotatingFileHandler
 import zmq
 import json
 
@@ -41,20 +39,11 @@ class MonitoringEventDispatcher(object):
 
     EVENTS_ENDPOINT = 'tcp://127.0.0.1:26891'
 
-    #rolling_handler = TimedRotatingFileHandler('/var/log/vismo_dispatch.log', backupCount=10, when='midnight')
-    #rolling_handler.doRollover()
-    # TRACE [2010-04-06 06:42:35,271] com.example.dw.Thing: Contemplating doing a thing.
-    #rolling_handler.setFormatter(logging.Formatter('%(levelname)s [%(asctime)-15s] %(clazz)s: %(message)s'))
-    #log = logging.getLogger('vismo')
-    #log.addHandler(rolling_handler)
-    #log.setLevel(logging.DEBUG)
 
-
-    def __init__(self):
+    def __init__(self, service_name):
+        self.service_name = service_name
         self.iface = 'eth0'
         self.ip = get_public_ip(self.iface)
-        #self.info('dispatcher startup, with pid={0}, ip={1}'.format(getpid(), self.iface + '/' + self.ip))
-        #self.debug('connecting to endpoint={0}'.format(MonitoringEventDispatcher.EVENTS_ENDPOINT))
         self.sock = self.create_push_socket(MonitoringEventDispatcher.EVENTS_ENDPOINT)
 
 
@@ -70,24 +59,7 @@ class MonitoringEventDispatcher(object):
     def send(self, **event):
         event['timestamp'] = int(1000 * time())
         event['originating-machine'] = self.ip
-        event['originating-service'] = 'object-service'
-        #self.debug('=> {0}'.format(event))
+        event['originating-service'] = self.service_name
+        event['cluster'] = 'test'
         self.sock.send(json.dumps(event))
-
-
-
-    def debug(self, msg):
-        MonitoringEventDispatcher.log.debug(msg, extra={'clazz':self.cls_name()})
-
-    def info(self, msg):
-        MonitoringEventDispatcher.log.info(msg, extra={'clazz':self.cls_name()})
-
-    def warn(self, msg):
-        MonitoringEventDispatcher.log.warn(msg, extra={'clazz':self.cls_name()})
-
-    def error(self, msg):
-        MonitoringEventDispatcher.log.error(msg, extra={'clazz':self.cls_name()})
-
-    def cls_name(self):
-        return self.__module__+ '.' + self.__class__.__name__
 
