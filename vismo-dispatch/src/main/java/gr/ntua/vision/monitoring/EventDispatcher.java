a9ee818ee1e07613fca33253896dc23fd2726d81
vassilis <alexv@mail.ntua.gr>
2012-08-30 15:38:01 +0300

    refactoring vismo-dispatch

--- vismo-dispatch/src/main/java/gr/ntua/vision/monitoring/EventDispatcher.java
+++ vismo-dispatch/src/main/java/gr/ntua/vision/monitoring/EventDispatcher.java
@@ -1,7 +1,6 @@
 package gr.ntua.vision.monitoring;
 
 import java.net.SocketException;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.json.simple.JSONValue;
@@ -12,50 +11,6 @@ import org.zeromq.ZMQ.Socket;
  * 
  */
 public class EventDispatcher {
-    /**
-     * This is a convenience object helping generate events.
-     */
-    public static class EventBuilder {
-        /** this is used to keep track of the event fields. */
-        private final Map<String, Object> dict = new HashMap<String, Object>();
-        /** the dispatcher. */
-        private final EventDispatcher     dispatcher;
-
-
-        /**
-         * Constructor.
-         * 
-         * @param dispatcher
-         *            the dispatcher.
-         */
-        public EventBuilder(final EventDispatcher dispatcher) {
-            this.dispatcher = dispatcher;
-        }
-
-
-        /**
-         * Append to the current event a new key/value pair.
-         * 
-         * @param key
-         *            the key.
-         * @param value
-         *            the value.
-         * @return <code>this</code>.
-         */
-        public EventBuilder field(final String key, final Object value) {
-            dict.put(key, value);
-            return this;
-        }
-
-
-        /**
-         * Send the event to the locally running <code>vismo</code> instance.
-         */
-        public void send() {
-            dispatcher.send(dict);
-        }
-    }
-
     /** the machine's external ip address. */
     private final String ip;
     /** the name of the service that generate events. */
