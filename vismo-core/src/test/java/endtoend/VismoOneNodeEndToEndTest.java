599b110fde3d9c03c47b87b378e9f7d44dfea208
vassilis <alexv@mail.ntua.gr>
2012-09-09 21:43:53 +0300

    start rewriting VismoOneNodeEndToEndTest

--- vismo-core/src/test/java/endtoend/VismoOneNodeEndToEndTest.java
+++ vismo-core/src/test/java/endtoend/VismoOneNodeEndToEndTest.java
@@ -1,9 +1,5 @@
 package endtoend;
 
-import gr.ntua.vision.monitoring.VismoConfiguration;
-import gr.ntua.vision.monitoring.notify.EventRegistry;
-import gr.ntua.vision.monitoring.zmq.ZMQSockets;
-
 import java.net.SocketException;
 import java.util.Properties;
 
@@ -12,11 +8,24 @@ import org.junit.Before;
 import org.junit.Test;
 import org.zeromq.ZContext;
 
+import examples.FakeEventConsumer;
+import gr.ntua.vision.monitoring.VismoConfiguration;
+import gr.ntua.vision.monitoring.notify.EventRegistry;
+import gr.ntua.vision.monitoring.zmq.ZMQSockets;
+
 
 /**
- *
+ * This test is used to demonstrate/verify Vismo's functionality in a simulated <strong>one node</strong> environment. For this
+ * purpose, a local event producer is used ({@link FakeObjectService}), the vismo main program, and a {@link FakeEventConsumer}.
+ * The workflow goes on like this:
+ * <ol>
+ * <li>Setting up the three different components, each running in each own thread.</li>
+ * <li>{@link FakeObjectService} generates and sends a number of events to the vismo.</li>
+ * <li>Vismo is responsible for aggregating the events for some period of time.</li>
+ * <li>Finally, {@link FakeEventConsumer} receives and verifies the events received.</li>
+ * </ol>
  */
-public class VismoEndToEndTest {
+public class VismoOneNodeEndToEndTest {
     /** the maximum number of events to sent for the test. */
     private static final int         NO_EVENTS_TO_SENT    = 10;
     /***/
