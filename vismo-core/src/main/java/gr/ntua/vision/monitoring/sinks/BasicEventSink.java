bb47c3b95f361c54b89e7a3124326a456393a54e
vassilis <alexv@mail.ntua.gr>
2012-10-08 18:07:01 +0300

    FIX: event sink does not send event topic

--- vismo-core/src/main/java/gr/ntua/vision/monitoring/sinks/BasicEventSink.java
+++ vismo-core/src/main/java/gr/ntua/vision/monitoring/sinks/BasicEventSink.java
@@ -10,79 +10,77 @@ import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * 
  */
 public class BasicEventSink implements EventSink {
-    /***/
-    private static final Logger   log      = LoggerFactory.getLogger(BasicEventSink.class);
-    /***/
-    protected final VismoSocket   sock;
-    // TODO: eventually remove eventIds or else vismo will blow with {@link OutOfMemoryError}.
-    /***/
-    private final HashSet<String> eventIds = new HashSet<String>();
+	/***/
+	private static final Logger log = LoggerFactory
+			.getLogger(BasicEventSink.class);
+	/***/
+	protected final VismoSocket sock;
+	// TODO: eventually remove eventIds or else vismo will blow with {@link
+	// OutOfMemoryError}.
+	/***/
+	private final HashSet<String> eventIds = new HashSet<String>();
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param sock
+	 */
+	public BasicEventSink(final VismoSocket sock) {
+		this.sock = sock;
+	}
 
-    /**
-     * Constructor.
-     * 
-     * @param sock
-     */
-    public BasicEventSink(final VismoSocket sock) {
-        this.sock = sock;
-    }
+	/**
+	 * @see gr.ntua.vision.monitoring.sinks.EventSink#send(gr.ntua.vision.monitoring.events.Event)
+	 */
+	@Override
+	public void send(final Event e) {
+		@SuppressWarnings("rawtypes")
+		final Map dict = (Map) e.get("!dict");
+		final String ser = JSONObject.toJSONString(dict);
 
+		if (!eventAlreadySent(dict)) {
+			log.trace("sending event: {}", ser);
+			send(e.topic(), ser);
+		}
+	}
 
-    /**
-     * @see gr.ntua.vision.monitoring.sinks.EventSink#send(gr.ntua.vision.monitoring.events.Event)
-     */
-    @Override
-    public void send(final Event e) {
-        @SuppressWarnings("rawtypes")
-        final Map dict = (Map) e.get("!dict");
-        final String ser = JSONObject.toJSONString(dict);
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "#<BasicEventSink using " + sock + ">";
+	}
 
-        if (!eventAlreadySent(dict)) {
-            log.trace("sending event: {}", ser);
-            send(ser);
-        }
-    }
+	/**
+	 * Check whether the event has already passed.
+	 * 
+	 * @param map
+	 * @return <code>true</code> iff the event has already been seen, according
+	 *         to its id, <code>false</code> otherwise.
+	 */
+	protected boolean eventAlreadySent(
+			@SuppressWarnings("rawtypes") final Map map) {
+		final String id = (String) map.get("id");
 
+		if (eventIds.contains(id)) {
+			log.error("dropping already sent event: {}", map);
+			return true;
+		}
 
-    /**
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-        return "#<BasicEventSink using " + sock + ">";
-    }
+		eventIds.add(id);
 
+		return false;
+	}
 
-    /**
-     * Check whether the event has already passed.
-     * 
-     * @param map
-     * @return <code>true</code> iff the event has already been seen, according to its id, <code>false</code> otherwise.
-     */
-    protected boolean eventAlreadySent(@SuppressWarnings("rawtypes") final Map map) {
-        final String id = (String) map.get("id");
-
-        if (eventIds.contains(id)) {
-            log.error("dropping already sent event: {}", map);
-            return true;
-        }
-
-        eventIds.add(id);
-
-        return false;
-    }
-
-
-    /**
-     * @param message
-     */
-    protected void send(final String message) {
-        sock.send(message);
-    }
+	/**
+	 * @param message
+	 */
+	protected void send(final String topic, final String message) {
+		sock.send(topic + " " + message);
+	}
 }
