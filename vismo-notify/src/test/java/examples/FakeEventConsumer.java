f2f71732ce2956e15abe5fa42332f78394362977
autre <b.alexandrou@gmail.com>
2012-10-11 17:15:28 +0300

    adding logging helpers

--- vismo-notify/src/test/java/examples/FakeEventConsumer.java
+++ vismo-notify/src/test/java/examples/FakeEventConsumer.java
@@ -6,38 +6,64 @@ import gr.ntua.vision.monitoring.notify.EventRegistry;
 
 import java.util.Map;
 
-
 /**
  *
  */
 public class FakeEventConsumer {
-    /**
+	private static final String SPECIAL_FIELD = "transaction-throughput";
+
+	private static class SAPLogger implements EventHandler {
+		/**
+		 * @see gr.ntua.vision.monitoring.notify.EventHandler#handle(gr.ntua.vision.monitoring.events.Event)
+		 */
+		@Override
+		public void handle(final Event e) {
+			try {
+				@SuppressWarnings("rawtypes")
+				final Map dict = (Map) e.get("!dict");
+				final String tenant = (String) e.get("tenant");
+				final Object special = e.get(SPECIAL_FIELD);
+
+				if (special != null && tenant != null && tenant.equalsIgnoreCase("sla_SAPIBI")) {
+					final String user = (String) e.get("user");
+
+					System.out.println(getClass().getSimpleName() + ": " + e.get("originating-machine") + " => " + dict);
+				}
+			} catch (final Throwable x) {
+				x.printStackTrace();
+			}
+		}
+	}
+
+	/**
      *
      */
-    public static class LoggingHandler implements EventHandler {
-        /**
-         * @see gr.ntua.vision.monitoring.notify.EventHandler#handle(gr.ntua.vision.monitoring.events.Event)
-         */
-        @Override
-        public void handle(final Event e) {
-            try {
-                @SuppressWarnings("rawtypes")
-                final Map dict = (Map) e.get("!dict");
+	public static class LoggingHandler implements EventHandler {
+		/**
+		 * @see gr.ntua.vision.monitoring.notify.EventHandler#handle(gr.ntua.vision.monitoring.events.Event)
+		 */
+		@Override
+		public void handle(final Event e) {
+			try {
+				@SuppressWarnings("rawtypes")
+				final Map dict = (Map) e.get("!dict");
+				final Object special = e.get(SPECIAL_FIELD);
 
-                System.out.println("=> " + dict);
-            } catch (final Throwable x) {
-                x.printStackTrace();
-            }
-        }
-    }
+				if (special != null)
+					System.err.println(getClass().getSimpleName() + ": " + e.get("originating-machine") + " => " + dict);
+			} catch (final Throwable x) {
+				x.printStackTrace();
+			}
+		}
+	}
 
+	/**
+	 * @param args
+	 */
+	public static void main(final String... args) {
+		final EventRegistry registry = new EventRegistry("tcp://10.0.1.103:56430");
 
-    /**
-     * @param args
-     */
-    public static void main(final String... args) {
-        final EventRegistry registry = new EventRegistry("tcp://10.0.1.103:56430");
-
-        registry.register("sla-per-request", new LoggingHandler());
-    }
+		registry.registerToAll(new SAPLogger());
+		registry.registerToAll(new LoggingHandler());
+	}
 }
