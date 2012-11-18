c514aed57d2212447ea4ddafa96c0b6444ba0a7c
vassilis <alexv@mail.ntua.gr>
2012-11-18 13:08:29 +0200

    cleaning up sinks

---
+++ vismo-core/src/main/java/gr/ntua/vision/monitoring/sinks/UniqueEventSink.java
@@ -0,0 +1,74 @@
+package gr.ntua.vision.monitoring.sinks;
+
+import gr.ntua.vision.monitoring.events.Event;
+import gr.ntua.vision.monitoring.zmq.VismoSocket;
+
+import java.util.HashSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * This is used to transmit events to a socket, if the event has not been encountered before.
+ */
+public class UniqueEventSink extends AbstractSink {
+    /***/
+    private static final Logger   log      = LoggerFactory.getLogger(UniqueEventSink.class);
+    // TODO: eventually remove eventIds or else vismo will blow with {@link OutOfMemoryError}.
+    /***/
+    private final HashSet<String> eventIds = new HashSet<String>();
+
+
+    /**
+     * Constructor.
+     * 
+     * @param sock
+     */
+    public UniqueEventSink(final VismoSocket sock) {
+        super(sock);
+    }
+
+
+    /**
+     * @see gr.ntua.vision.monitoring.sinks.EventSink#send(gr.ntua.vision.monitoring.events.Event)
+     */
+    @Override
+    public void send(final Event e) {
+        sendOrDrop(e);
+    }
+
+
+    /**
+     * Check that we have not seen an event with the same id.
+     * 
+     * @param id
+     *            the event id.
+     * @return <code>true</code> iff we have not seen the id.
+     */
+    private boolean haveSeenEvent(final String id) {
+        return eventIds.contains(id);
+    }
+
+
+    /**
+     * Serialize and transmit the event.
+     * 
+     * @param e
+     *            the event to send.
+     */
+    private void sendOrDrop(final Event e) {
+        final String id = (String) e.get("id");
+
+        if (haveSeenEvent(id)) {
+            log.error("dropping already sent event with id: {}", id);
+            return;
+        }
+
+        eventIds.add(id);
+        final String s = serialize(e);
+
+        log.trace("sending event: {}", s);
+        send(s);
+    }
+}
