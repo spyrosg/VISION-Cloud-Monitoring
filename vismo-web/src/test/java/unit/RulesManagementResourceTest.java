0778729ac6e86457ef2e5e84afd1e241199ea530
thodoris <thodoris.messinis@gmail.com>
2012-11-26 19:39:54 +0200

    junit tests

---
+++ vismo-web/src/test/java/unit/RulesManagementResourceTest.java
@@ -0,0 +1,117 @@
+package unit;
+
+import static com.eclipsesource.restfuse.Assert.assertOk;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+import com.eclipsesource.restfuse.Destination;
+import com.eclipsesource.restfuse.HttpJUnitRunner;
+import com.eclipsesource.restfuse.Method;
+import com.eclipsesource.restfuse.Response;
+import com.eclipsesource.restfuse.annotation.Context;
+import com.eclipsesource.restfuse.annotation.HttpTest;
+
+import gr.ntua.vision.monitoring.web.Server;
+
+
+@RunWith(HttpJUnitRunner.class)
+public class RulesManagementResourceTest {
+    static Server server = new Server();
+
+
+    @BeforeClass
+    public static void startServer() throws IllegalArgumentException, IOException {
+        server.start();
+    }
+
+    @Rule
+    public Destination restfuse = new Destination("http://localhost:9998");
+
+    @Context
+    private Response   response;
+
+
+    @HttpTest(method = Method.PUT, path = "rules/Aggregation-default/1/The-default1-aggregation")
+    public void checkRestPutRule1() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.PUT, path = "rules/Aggregation-rule2/2/A-second-aggregation-rule")
+    public void checkRestPutRule2() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.PUT, path = "rules/Aggregation-rule3/3/A-third-aggregation-rule")
+    public void checkRestPutRule3() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.PUT, path = "rules/Aggregation-rule4/4/A-fourth-aggregation-rule")
+    public void checkRestPutRule4() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.GET, path = "rules/1")
+    public void checkRestGetRule1() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.GET, path = "rules/2")
+    public void checkRestGetRule2() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.GET, path = "rules/3")
+    public void checkRestGetRule3() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.GET, path = "rules/4")
+    public void checkRestGetRule4() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.DELETE, path = "rules/1")
+    public void checkRestDeleteRule1() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.DELETE, path = "rules/2")
+    public void checkRestDeleteRule2() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.DELETE, path = "rules/3")
+    public void checkRestDeleteRule3() {
+        assertOk(response);
+    }
+
+
+    @HttpTest(method = Method.DELETE, path = "rules/4")
+    public void checkRestDeleteRule4() {
+        assertOk(response);
+    }
+
+    /*
+    @AfterClass
+    public static void stopServer() throws IllegalArgumentException, IOException {
+        server.stop();
+    }
+    */
+
+}
