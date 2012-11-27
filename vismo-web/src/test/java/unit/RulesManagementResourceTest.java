3ccf4b29d6eb758a86b622de2268ad1bf8f855e4
thodoris <thodoris.messinis@gmail.com>
2012-11-27 16:23:35 +0200

    simplify test

--- vismo-web/src/test/java/unit/RulesManagementResourceTest.java
+++ vismo-web/src/test/java/unit/RulesManagementResourceTest.java
@@ -5,7 +5,9 @@ import static com.eclipsesource.restfuse.Assert.assertNoContent;
 
 import java.io.IOException;
 
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
@@ -24,11 +26,10 @@ import gr.ntua.vision.monitoring.web.Server;
 public class RulesManagementResourceTest {
     static Server server = new Server();
 
-    
 
     @BeforeClass
     public static void startServer() throws IllegalArgumentException, IOException {
-      server.start();
+        server.start();
     }
 
     @Rule
@@ -43,33 +44,6 @@ public class RulesManagementResourceTest {
      */
     @HttpTest(method = Method.PUT, path = "rules/Aggregation-default/1/The-default1-aggregation")
     public void checkRestPutRule1() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the insertion of a rule
-     */
-    @HttpTest(method = Method.PUT, path = "rules/Aggregation-rule2/2/A-second-aggregation-rule")
-    public void checkRestPutRule2() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the insertion of a rule
-     */
-    @HttpTest(method = Method.PUT, path = "rules/Aggregation-rule3/3/A-third-aggregation-rule")
-    public void checkRestPutRule3() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the insertion of a rule
-     */
-    @HttpTest(method = Method.PUT, path = "rules/Aggregation-rule4/4/A-fourth-aggregation-rule")
-    public void checkRestPutRule4() {
         assertOk(response);
     }
 
@@ -97,87 +71,6 @@ public class RulesManagementResourceTest {
      */
     @HttpTest(method = Method.GET, path = "rules/1")
     public void checkRestGetRule1AfterDelete() {
-        assertNoContent(response);
-    }
-
-
-    /**
-     * checks the retrieval of a rule
-     */
-    @HttpTest(method = Method.GET, path = "rules/2")
-    public void checkRestGetRule2() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the deletion of a rule
-     */
-    @HttpTest(method = Method.DELETE, path = "rules/2")
-    public void checkRestDeleteRule2() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the retrieval of a rule after deletion
-     */
-    @HttpTest(method = Method.GET, path = "rules/2")
-    public void checkRestGetRule2AfterDelete() {
-        assertNoContent(response);
-    }
-
-
-    /**
-     * checks the retrieval of a rule
-     */
-    @HttpTest(method = Method.GET, path = "rules/3")
-    public void checkRestGetRule3() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the deletion of a rule
-     */
-    @HttpTest(method = Method.DELETE, path = "rules/3")
-    public void checkRestDeleteRule3() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the retrieval of a rule after deletion
-     */
-    @HttpTest(method = Method.GET, path = "rules/3")
-    public void checkRestGetRule3AfterDelete() {
-        assertNoContent(response);
-    }
-
-
-    /**
-     * checks the retrieval of a rule
-     */
-    @HttpTest(method = Method.GET, path = "rules/4")
-    public void checkRestGetRule4() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the deletion of a rule
-     */
-    @HttpTest(method = Method.DELETE, path = "rules/4")
-    public void checkRestDeleteRule4() {
-        assertOk(response);
-    }
-
-
-    /**
-     * checks the retrieval of a rule after deletion
-     */
-    @HttpTest(method = Method.GET, path = "rules/4")
-    public void checkRestGetRule4AfterDelete() {
         assertNoContent(response);
     }
 
