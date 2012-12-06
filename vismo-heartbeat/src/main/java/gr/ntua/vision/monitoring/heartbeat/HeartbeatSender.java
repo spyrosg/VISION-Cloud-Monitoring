4bd9f6197cc0560c207a90f5866aceb99f5ba355
thodoris <mthodor@mail.ntua.gr>
2012-12-06 15:43:14 +0200

    fixing identation

--- vismo-heartbeat/src/main/java/gr/ntua/vision/monitoring/heartbeat/HeartbeatSender.java
+++ vismo-heartbeat/src/main/java/gr/ntua/vision/monitoring/heartbeat/HeartbeatSender.java
@@ -58,6 +58,7 @@ public final class HeartbeatSender {
         this.timeToLive = timeToLive;
     }
 
+
     /**
      * Start the heartbeat thread
      */
@@ -82,6 +83,7 @@ public final class HeartbeatSender {
     private final class MulticastSenderThread extends Thread {
         private MulticastSocket socket;
 
+
         public final void run() {
             while (!stopped) {
                 try {
@@ -92,139 +94,144 @@ public final class HeartbeatSender {
                     while (!stopped) {
                         byte[] buffer = createPayload();
                         DatagramPacket packet = new DatagramPacket(buffer, buffer.length, groupMulticastAddress,
-                                                                   groupMulticastPort.intValue());
+                                groupMulticastPort.intValue());
                         socket.send(packet);
 
                         try {
                             wait(heartBeatInterval);
-                        
-                    } catch (InterruptedException e) {
-                        if (!stopped) {
-                            log.info("Error receiving heartbeat. Initial cause was " + e.getMessage(), e);
+
+                        } catch (InterruptedException e) {
+                            if (!stopped) {
+                                log.info("Error receiving heartbeat. Initial cause was " + e.getMessage(), e);
+                            }
                         }
                     }
+                } catch (IOException e) {
+                    log.info("Error on multicast socket", e);
+                } catch (Throwable e) {
+                    log.info("Unexpected throwable in run thread. Continuing..." + e.getMessage(), e);
+                } finally {
+                    closeSocket();
                 }
-            } catch (IOException e) {
-                log.info("Error on multicast socket", e);
-            } catch (Throwable e) {
-                log.info("Unexpected throwable in run thread. Continuing..." + e.getMessage(), e);
-            } finally {
-                closeSocket();
-            }
-            if (!stopped) {
-                try {
-                    sleep(heartBeatInterval);
-                } catch (InterruptedException e) {
-                    log.info("Sleep after error interrupted. Initial cause was " + e.getMessage(), e);
-                }
-            }
-        }
-    }
-
-    /**
-     * create the multicast packet payload.
-     * 
-     * @throws SocketException
-     * @throws UnknownHostException
-     */
-    private byte[] createPayload() throws UnknownHostException, SocketException {
-        String interfaceIp = getHostIP();
-        byte[] msg = interfaceIp.getBytes();
-        return msg;
-    }
-
-    /**
-     * gets the ip addresses of the node interfaces.
-     * 
-     * @return
-     * @throws UnknownHostException
-     * @throws SocketException
-     */
-    public String getHostIP() throws UnknownHostException, SocketException {
-        String hostAddress = "";
-        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
-            NetworkInterface intf = en.nextElement();
-            for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
-                InetAddress inetAddress = enumIpAddr.nextElement();
-                if (!inetAddress.isLoopbackAddress()) {
-                    hostAddress = buildIPAllInterfacesString(hostAddress, inetAddress);
+                if (!stopped) {
+                    try {
+                        sleep(heartBeatInterval);
+                    } catch (InterruptedException e) {
+                        log.info("Sleep after error interrupted. Initial cause was " + e.getMessage(), e);
+                    }
                 }
             }
         }
-        return hostAddress;
-    }
-
-    /*
-     * 
-     */
-    private String buildIPAllInterfacesString(String ipString, InetAddress inetAddress) {
-        if (ipString.isEmpty())
-            ipString = inetAddress.toString().substring(1);
-        else
-            ipString = ipString + ":" + inetAddress.toString().substring(1);
-        return ipString;
-    }
 
 
-    /*
-     * @see java.lang.Thread#interrupt()
-     */
-    public final void interrupt() {
-        closeSocket();
-        super.interrupt();
-    }
+        /**
+         * create the multicast packet payload.
+         * 
+         * @throws SocketException
+         * @throws UnknownHostException
+         */
+        private byte[] createPayload() throws UnknownHostException, SocketException {
+            String interfaceIp = getHostIP();
+            byte[] msg = interfaceIp.getBytes();
+            return msg;
+        }
 
 
-    private void closeSocket() {
-        try {
-            if (socket != null && !socket.isClosed()) {
+        /**
+         * gets the ip addresses of the node interfaces.
+         * 
+         * @return
+         * @throws UnknownHostException
+         * @throws SocketException
+         */
+        public String getHostIP() throws UnknownHostException, SocketException {
+            String hostAddress = "";
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        hostAddress = buildIPAllInterfacesString(hostAddress, inetAddress);
+                    }
+                }
+            }
+            return hostAddress;
+        }
+
+
+        /*
+         * 
+         */
+        private String buildIPAllInterfacesString(String ipString, InetAddress inetAddress) {
+            if (ipString.isEmpty())
+                ipString = inetAddress.toString().substring(1);
+            else
+                ipString = ipString + ":" + inetAddress.toString().substring(1);
+            return ipString;
+        }
+
+
+        /*
+         * @see java.lang.Thread#interrupt()
+         */
+        public final void interrupt() {
+            closeSocket();
+            super.interrupt();
+        }
+
+
+        private void closeSocket() {
+            try {
+                if (socket != null && !socket.isClosed()) {
+                    try {
+                        socket.leaveGroup(groupMulticastAddress);
+                    } catch (IOException e) {
+                        log.info("Error leaving multicast group. Message was " + e.getMessage());
+                    }
+                    socket.close();
+                }
+            } catch (NoSuchMethodError e) {
+                log.info("socket.isClosed is not supported by JDK");
                 try {
                     socket.leaveGroup(groupMulticastAddress);
-                } catch (IOException e) {
-                    log.info("Error leaving multicast group. Message was " + e.getMessage());
+                } catch (IOException ex) {
+                    log.info("Error leaving multicast group. Message was " + ex.getMessage());
                 }
                 socket.close();
             }
-        } catch (NoSuchMethodError e) {
-            log.info("socket.isClosed is not supported by JDK");
-            try {
-                socket.leaveGroup(groupMulticastAddress);
-            } catch (IOException ex) {
-                log.info("Error leaving multicast group. Message was " + ex.getMessage());
-            }
-            socket.close();
+        }
+
+    }
+
+
+    /**
+     * Sets the heartbeat interval to something other than the default of 2000ms.
+     * 
+     * @param heartBeatInterval
+     *            a time in ms, greater than 1000
+     */
+    public static void setHeartBeatInterval(long heartBeatInterval) {
+        if (heartBeatInterval < MINIMUM_HEARTBEAT_INTERVAL) {
+            log.info("Trying to set heartbeat interval too low. Using MINIMUM_HEARTBEAT_INTERVAL instead.");
+            HeartbeatSender.heartBeatInterval = MINIMUM_HEARTBEAT_INTERVAL;
+        } else {
+            HeartbeatSender.heartBeatInterval = heartBeatInterval;
         }
     }
 
-}
 
-
-/**
- * Sets the heartbeat interval to something other than the default of 2000ms.
- * 
- * @param heartBeatInterval
- *            a time in ms, greater than 1000
- */
-public static void setHeartBeatInterval(long heartBeatInterval) {
-    if (heartBeatInterval < MINIMUM_HEARTBEAT_INTERVAL) {
-        log.info("Trying to set heartbeat interval too low. Using MINIMUM_HEARTBEAT_INTERVAL instead.");
-        HeartbeatSender.heartBeatInterval = MINIMUM_HEARTBEAT_INTERVAL;
-    } else {
-        HeartbeatSender.heartBeatInterval = heartBeatInterval;
+    /**
+     * Returns the heartbeat interval.
+     */
+    public static long getHeartBeatInterval() {
+        return heartBeatInterval;
     }
-}
 
-/**
- * Returns the heartbeat interval.
- */
-public static long getHeartBeatInterval() {
-    return heartBeatInterval;
-}
 
-/**
- * @return the TTL
- */
-public Integer getTimeToLive() {
-    return timeToLive;
-}
+    /**
+     * @return the TTL
+     */
+    public Integer getTimeToLive() {
+        return timeToLive;
+    }
 }
