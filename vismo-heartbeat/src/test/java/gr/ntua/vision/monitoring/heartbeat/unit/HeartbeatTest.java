89641a6c86c6b27b2e54ad1d077e826f3dd53b70
thodoris <mthodor@mail.ntua.gr>
2012-12-07 15:06:22 +0200

    adding heartbeat test in a different package

---
+++ vismo-heartbeat/src/test/java/gr/ntua/vision/monitoring/heartbeat/unit/HeartbeatTest.java
@@ -0,0 +1,103 @@
+package gr.ntua.vision.monitoring.heartbeat.unit;
+
+import gr.ntua.vision.monitoring.heartbeat.HeartbeatReceiver;
+import gr.ntua.vision.monitoring.heartbeat.HeartbeatSender;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HeartbeatTest {
+
+    private static final String MULTICAST_IP   = "224.0.0.2";
+    private static final int    MULTICAST_PORT = 6000;
+    private static final int    TTL            = 0;
+    
+    private static final Logger   log                        = LoggerFactory.getLogger(HeartbeatSender.class);
+
+
+    /*
+     * 
+     */
+    @Test
+    public void testHeartbeatServiceSuccessfull() throws IOException {
+        log.info("starting HeartbeatServiceSuccessfull test...");
+        
+
+        final HeartbeatReceiver receiver = new HeartbeatReceiver(InetAddress.getByName(HeartbeatTest.MULTICAST_IP),
+                HeartbeatTest.MULTICAST_PORT);
+        receiver.clearMembership();
+        final HeartbeatSender sender = new HeartbeatSender(InetAddress.getByName(HeartbeatTest.MULTICAST_IP),
+                HeartbeatTest.MULTICAST_PORT, HeartbeatTest.TTL);
+        sender.setHeartBeatInterval(1000);
+        receiver.init();
+        sender.init();
+        sleep(2000);
+        Assert.assertEquals("We expect membership to be valid", true, checkMembership(receiver.getMembers()));
+        sender.halt();
+        receiver.halt();
+    }
+
+
+    /*
+     * 
+     */
+    @Test
+    public void testHeartbeatServiceUnSuccessfull() throws IOException {
+        log.info("starting HeartbeatServiceUnSuccessfull test...");
+
+        final HeartbeatReceiver receiver = new HeartbeatReceiver(InetAddress.getByName(HeartbeatTest.MULTICAST_IP),
+                HeartbeatTest.MULTICAST_PORT);
+        receiver.clearMembership();
+        final HeartbeatSender sender = new HeartbeatSender(InetAddress.getByName(HeartbeatTest.MULTICAST_IP),
+                HeartbeatTest.MULTICAST_PORT, HeartbeatTest.TTL);
+        sender.setHeartBeatInterval(10000);
+        receiver.init();
+        sender.init();
+        sleep(4000);
+        Assert.assertEquals("We expect membership to be invalid", false, checkMembership(receiver.getMembers()));
+        sender.halt();
+        receiver.halt();
+    }
+
+
+    /*
+     * 
+     */
+    private boolean checkMembership(final HashMap<String, Boolean> members) {
+
+        boolean result = true;
+
+        if (members.size() == 0)
+            return false;
+        else {
+            final Iterator<String> iterator = members.keySet().iterator();
+            while (iterator.hasNext())
+                result = result && members.get(iterator.next().toString()).booleanValue();
+            return result;
+
+        }
+
+    }
+
+
+    /*
+     * 
+     */
+    private void sleep(final long time) {
+        try {
+            Thread.sleep(time);
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
