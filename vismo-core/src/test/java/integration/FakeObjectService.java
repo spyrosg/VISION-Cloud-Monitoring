bd8c4ceb6b1816296fb26493d50de6ba29320f6e
vassilis <alexv@mail.ntua.gr>
2013-01-03 14:21:16 +0200

    adding VismoService integration test

---
+++ vismo-core/src/test/java/integration/FakeObjectService.java
@@ -0,0 +1,52 @@
+package integration;
+
+import gr.ntua.vision.monitoring.VismoEventDispatcher;
+
+
+/**
+ * 
+ */
+public class FakeObjectService {
+    /***/
+    private final VismoEventDispatcher dispatcher;
+
+
+    /**
+     * Constructor.
+     * 
+     * @param dispatcher
+     */
+    public FakeObjectService(final VismoEventDispatcher dispatcher) {
+        this.dispatcher = dispatcher;
+    }
+
+
+    /**
+     * @param noReadEvents
+     */
+    public void sendReadEvents(final int noReadEvents) {
+        for (int i = 0; i < noReadEvents; ++i)
+            dispatcher.newEvent().field("object", "foo").field("container", "bar").field("user", "bill").field("tenant", "ntua")
+                    .field("operation", "GET").field("content-size", 1000).send();
+
+    }
+
+
+    /**
+     * @param noWriteEvents
+     */
+    public void sendWriteEvents(final int noWriteEvents) {
+        for (int i = 0; i < noWriteEvents; ++i)
+            dispatcher.newEvent().field("object", "foo").field("container", "bar").field("user", "bill").field("tenant", "ntua")
+                    .field("operation", "PUT").field("content-size", 1000).send();
+
+    }
+
+
+    /**
+     * 
+     */
+    public void start() {
+
+    }
+}
---
+++ vismo-core/src/test/java/integration/VismoServiceTest.java
@@ -0,0 +1,144 @@
+package integration;
+
+import static org.junit.Assert.assertEquals;
+import gr.ntua.vision.monitoring.VismoConfiguration;
+import gr.ntua.vision.monitoring.VismoEventDispatcher;
+import gr.ntua.vision.monitoring.VismoVMInfo;
+import gr.ntua.vision.monitoring.events.Event;
+import gr.ntua.vision.monitoring.notify.VismoEventRegistry;
+import gr.ntua.vision.monitoring.rules.Rule;
+import gr.ntua.vision.monitoring.rules.VismoRulesEngine;
+import gr.ntua.vision.monitoring.service.Service;
+import gr.ntua.vision.monitoring.service.WorkerNodeFactory;
+import gr.ntua.vision.monitoring.zmq.VismoSocket;
+import gr.ntua.vision.monitoring.zmq.ZMQSockets;
+
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.zeromq.ZContext;
+
+
+/**
+ * 
+ */
+public class VismoServiceTest {
+    /**
+     * 
+     */
+    private final class EventCounter extends Rule {
+        /***/
+        private int counter = 0;
+
+
+        /**
+         * Constructor.
+         * 
+         * @param engine
+         */
+        public EventCounter(final VismoRulesEngine engine) {
+            super(engine);
+        }
+
+
+        /**
+         * @param expectedNoEvents
+         */
+        public void hasSeenExpectedNoEvents(final int expectedNoEvents) {
+            assertEquals(expectedNoEvents, counter);
+        }
+
+
+        /**
+         * @see gr.ntua.vision.monitoring.rules.RuleProc#performWith(java.lang.Object)
+         */
+        @Override
+        public void performWith(final Event e) {
+            if (e != null)
+                ++counter;
+        }
+    }
+
+    /***/
+    private static final int   NO_READ_EVENTS_TO_SEND  = 10;
+    /***/
+    private static final int   NO_WRITE_EVENTS_TO_SEND = 10;
+    /***/
+    EventCounter               counterRule;
+    /***/
+    private VismoConfiguration conf;
+    /***/
+    private FakeObjectService  obs;
+    /***/
+    @SuppressWarnings("serial")
+    private final Properties   p                       = new Properties() {
+                                                           {
+                                                               setProperty("cloud.name", "visioncloud.eu");
+                                                               setProperty("cloud.heads", "10.0.2.211, 10.0.2.212");
+
+                                                               setProperty("cluster.name", "vision-1");
+                                                               setProperty("cluster.head", "10.0.2.211");
+
+                                                               setProperty("producers.point", "tcp://127.0.0.1:56429");
+                                                               setProperty("consumers.port", "56430");
+
+                                                               setProperty("udp.port", "56431");
+                                                               setProperty("cluster.head.port", "56432");
+
+                                                               setProperty("cloud.head.port", "56433");
+                                                           }
+                                                       };
+    /***/
+    private VismoEventRegistry reg;
+    /***/
+    private Service            service;
+    /***/
+    private WorkerNodeFactory  serviceFactory;
+    /***/
+    private final VismoVMInfo  vminfo                  = new VismoVMInfo();
+    /***/
+    private final ZMQSockets   zmq                     = new ZMQSockets(new ZContext());
+
+
+    /***/
+    @Test
+    public void receivesExpectedEvents() {
+        service.start();
+        obs.start();
+
+        obs.sendReadEvents(NO_READ_EVENTS_TO_SEND);
+        obs.sendWriteEvents(NO_WRITE_EVENTS_TO_SEND);
+
+        assertThatServiceReceivedAllEvents(NO_READ_EVENTS_TO_SEND + NO_WRITE_EVENTS_TO_SEND);
+    }
+
+
+    /***/
+    @Before
+    public void setUp() {
+        conf = new VismoConfiguration(p);
+
+        serviceFactory = new WorkerNodeFactory(conf, zmq) {
+            @Override
+            protected void boostrap(final VismoRulesEngine engine) {
+                super.boostrap(engine);
+                counterRule = new EventCounter(engine);
+                counterRule.submit();
+            }
+        };
+
+        final VismoSocket sock = zmq.newConnectedPushSocket(conf.getProducersPoint());
+        obs = new FakeObjectService(new VismoEventDispatcher("fake-obs", sock));
+        System.err.println("vismo dispatcher using: " + sock);
+        service = serviceFactory.build(vminfo);
+    }
+
+
+    /**
+     * @param noExpectedEvents
+     */
+    private void assertThatServiceReceivedAllEvents(final int noExpectedEvents) {
+        counterRule.hasSeenExpectedNoEvents(noExpectedEvents);
+    }
+}
