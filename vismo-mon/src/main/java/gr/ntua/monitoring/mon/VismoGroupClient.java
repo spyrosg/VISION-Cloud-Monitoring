234639706e6bbf11421f29b6fc375293cb15885e
vassilis <alexv@mail.ntua.gr>
2013-01-18 23:39:15 +0200

    adding VismoGroup{Client,Server}

---
+++ vismo-mon/src/main/java/gr/ntua/monitoring/mon/VismoGroupClient.java
@@ -0,0 +1,34 @@
+package gr.ntua.monitoring.mon;
+
+import gr.ntua.vision.monitoring.VismoConfiguration;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+
+/**
+ *
+ */
+public class VismoGroupClient {
+    /***/
+    private final GroupClient client;
+
+
+    /**
+     * @param conf
+     * @throws UnknownHostException
+     */
+    public VismoGroupClient(final VismoConfiguration conf) throws UnknownHostException {
+        this.client = new GroupClient(conf.getMonGroupAddr(), conf.getMonGroupPort());
+    }
+
+
+    /**
+     * @param note
+     * @throws IOException
+     * @see gr.ntua.monitoring.mon.GroupClient#notifyGroup(java.lang.String)
+     */
+    public void notifyGroup(final String note) throws IOException {
+        client.notifyGroup(note);
+    }
+}
