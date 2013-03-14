f59052f7c7a284dcd24ee10e0aa0f21e74bf5852
vassilis <alexv@mail.ntua.gr>
2013-03-14 19:30:30 +0200

    cleaning up; refactoring in progress

--- vismo-dispatch/src/main/python/vismo_dispatch.py
+++ vismo-dispatch/src/main/python/vismo_dispatch.py
@@ -12,14 +12,12 @@ import zmq
 import json
 
 
-# this is the file that holds the lib's configuration
+## this is the file that holds the lib's configuration
 CONFIGURATION_PROPERTIES = getenv('VISMO_CONFIG', '/etc/visioncloud_vismo.conf')
 
-###
 ##
 ## Utilities
 ##
-###
 
 def get_public_ip(iface='eth0'):
     """
@@ -53,55 +51,24 @@ def log_and_raise(msg, excp):
     print('vismo-local-event-dispatcher: {0}'.format(msg), file=sys.stderr)
     raise excp(msg)
 
-_MAX_LENGTH = 80
-def safe_repr(obj, short=False):
-    try:
-        result = repr(obj)
-    except Exception:
-        result = object.__repr__(obj)
-    if not short or len(result) < _MAX_LENGTH:
-        return result
-    return result[:_MAX_LENGTH] + ' [truncated]...'
-
-
-
+##
 ## core
+##
 
 class EventDispatcher(object):
     """
-        An event dispatcher is used to pass around events to
-        interested parties.
+        An event dispatcher is used to pass events down a socket.
+        An event is just a collection of key/value pairs (a python dict).
     """
 
     def __init__(self, service_name, sock):
         self.service_name = service_name
         self.sock = sock
 
-
     def send(self, **event):
-        """Actually dispatch the event."""
+        """Dispatch the event."""
 
         self.sock.send(event)
-
-
-    def calculate_event_time_difference(self, e1, e2):
-        """
-            Calculates the time difference of the two events. It is assumed
-            that e1 ``happens before'' e2, so that a positive difference is
-            returned.
-
-            It is also assumed that the event timestamps are in millis and
-            the returned value is in seconds.
-        """
-
-        return (float(e2['timestamp']) - float(e1['timestamp'])) / 1000.0
-
-
-    def calculate_mean_value_per_time_unit(self, val, time_diff):
-        if is_effectively_zero(time_diff):
-            return 0.0
-
-        return float(val) / float(time_diff)
 
 
 class VismoEventDispatcher(EventDispatcher):
@@ -117,7 +84,6 @@ class VismoEventDispatcher(EventDispatcher):
         to the same request/response.
     """
 
-
     def __init__(self, service_name):
         self.load_configuration()
         sock = self.create_push_socket(self.producers_point)
@@ -125,6 +91,7 @@ class VismoEventDispatcher(EventDispatcher):
         self.start_request_event = None
         self.start_response_event = None
         self.end_response_event = None
+        self.events_received = []
 
     def load_configuration(self):
         p = Properties()
@@ -144,6 +111,23 @@ class VismoEventDispatcher(EventDispatcher):
 
         return sock
 
+    def calculate_event_time_difference(self, e1, e2):
+        """
+            Calculates the time difference of the two events. It is assumed
+            that e1 ``happens before'' e2, so that a positive difference is
+            returned.
+
+            It is also assumed that the event timestamps are in millis and
+            the returned value is in seconds.
+        """
+
+        return (float(e2['timestamp']) - float(e1['timestamp'])) / 1000.0
+
+    def calculate_mean_value_per_time_unit(self, val, time_diff):
+        if is_effectively_zero(time_diff):
+            return 0.0
+
+        return float(val) / float(time_diff)
 
     def send(self, **event):
         """
@@ -152,9 +136,7 @@ class VismoEventDispatcher(EventDispatcher):
 
         self.cleanup_event(event)
         self.add_basic_fields(event)
-        log('sending: {0}'.format(event))
         self.handle_event(event)
-
 
     def cleanup_event(self, event):
         """
@@ -189,7 +171,6 @@ class VismoEventDispatcher(EventDispatcher):
             event['operation'] = object_service_topic_to_http_op(event['topic'])
             del event['topic']
 
-
     def add_basic_fields(self, event):
         event['timestamp'] = int(1000 * time())
         event['originating-machine'] = self.ip
@@ -198,8 +179,8 @@ class VismoEventDispatcher(EventDispatcher):
         event['id'] = str(uuid4())
 
     def _sock_send(self, event):
+        log('sending: {0}'.format(event))
         self.sock.send(json.dumps(event))
-
 
     def handle_event(self, event):
         """
@@ -248,7 +229,6 @@ class VismoEventDispatcher(EventDispatcher):
             # send anyway
             self._sock_send(event)
 
-
     def calculate_latency(self):
         """
             Latency here is defined as the time duration from the time the system received
@@ -258,7 +238,6 @@ class VismoEventDispatcher(EventDispatcher):
 
         return self.calculate_event_time_difference(self.start_request_event, self.start_response_event)
 
-
     def calculate_transaction_duration(self):
         """
             Transaction duration is the time spent serving the request. More accurately, is the
@@ -267,7 +246,6 @@ class VismoEventDispatcher(EventDispatcher):
         """
 
         return self.calculate_event_time_difference(self.start_request_event, self.end_response_event)
-
 
     def calculate_throughput(self, content_size, transaction_time):
         """
@@ -279,136 +257,27 @@ class VismoEventDispatcher(EventDispatcher):
         return self.calculate_mean_value_per_time_unit(content_size, transaction_time)
 
 
-
-
-
 if __name__ == '__main__':
-    import unittest
     from time import sleep
 
-    class MyTestCase(unittest.TestCase):
-        """
-            This is used to reproduce to methods missing from
-            python 2.6.6
-        """
-
-        def assertGreater(self, a, b, msg=None):
-            """Just like self.assertTrue(a > b), but with a nicer default message."""
-            if not a > b:
-                standardMsg = '%s not greater than %s' % (safe_repr(a), safe_repr(b))
-                self.fail(self._formatMessage(msg, standardMsg))
-
-        def assertAlmostEqual(self, first, second, places=None, msg=None, delta=None):
-            """Fail if the two objects are unequal as determined by their
-               difference rounded to the given number of decimal places
-               (default 7) and comparing to zero, or by comparing that the
-               between the two objects is more than the given delta.
-
-               Note that decimal places (from zero) are usually not the same
-               as significant digits (measured from the most signficant digit).
-
-               If the two objects compare equal then they will automatically
-               compare almost equal.
-            """
-            if first == second:
-                # shortcut
-                return
-            if delta is not None and places is not None:
-                raise TypeError("specify delta or places not both")
-
-            if delta is not None:
-                if abs(first - second) <= delta:
-                    return
-
-                standardMsg = '%s != %s within %s delta' % (safe_repr(first),
-                                                            safe_repr(second),
-                                                            safe_repr(delta))
-            else:
-                if places is None:
-                    places = 7
-
-                if round(abs(second-first), places) == 0:
-                    return
-
-                standardMsg = '%s != %s within %r places' % (safe_repr(first),
-                                                              safe_repr(second),
-                                                              places)
-            msg = self._formatMessage(msg, standardMsg)
-            raise self.failureException(msg)
-
-
-    class EventDispatcherTest(MyTestCase):
-        def setUp(self):
-            self.content_size = 1000 # in bytes
-            self.obj = 'ofdesire'
-            self.success_status = 'SUCCESS'
-            self.fail_status = 'FAIL'
-            self.time_till_start_of_response = 0.1 # in seconds
-            self.time_till_end_of_response = 0.1 # in seconds
-            self.delta = 0.005
-            self.sent_events = []
-            self.sock = object()
-            def sock(): pass
-            sock.send = lambda e: self.sent_events.append(e)
-            self.dispatcher = EventDispatcher('foo', sock)
-
-        def send_event(self, **args):
-            args['timestamp'] = int(1000 * time())
-            args['content-size'] = args['content_size']
-            del args['content_size']
-            self.dispatcher.send(**args)
-
-        def send_start_request(self):
-            self.send_event(tag='start-request', content_size=self.content_size, obj=self.obj, status=self.success_status)
-
-        def send_start_response(self):
-            self.send_event(tag='start-response', content_size=self.content_size, obj=self.obj, status=self.success_status)
-
-        def send_end_response(self):
-            self.send_event(tag='end-response', content_size=self.content_size, obj=self.obj, status=self.success_status)
-
-        def perform_full_request_response_event_generation(self):
-            self.send_start_request()
-            sleep(self.time_till_start_of_response)
-            self.send_start_response()
-            sleep(self.time_till_end_of_response)
-            self.send_end_response()
-
-
-        def test_that_event_was_sent(self):
-            self.send_event(tag='start-request', content_size=1000, obj='ofdesire', status=1)
-            self.assertEquals(self.content_size, self.sent_events[0]['content-size'])
-
-
-        def test_event_latency(self):
-            self.perform_full_request_response_event_generation()
-
-            latency = self.dispatcher.calculate_event_time_difference(self.sent_events[0], self.sent_events[1])
-            self.assertGreater(latency, 0)
-            self.assertAlmostEqual(self.time_till_start_of_response, latency, delta=self.delta)
-
-
-        def test_event_throughput(self):
-            self.perform_full_request_response_event_generation()
-
-            throughput = self.dispatcher.calculate_mean_value_per_time_unit(self.sent_events[0]['content-size'], self.time_till_start_of_response + self.time_till_end_of_response)
-            self.assertAlmostEqual(self.content_size / (self.time_till_start_of_response + self.time_till_end_of_response), throughput, delta=self.delta)
-
-
-        def test_event_transaction_time(self):
-            self.perform_full_request_response_event_generation()
-
-            transaction_time = self.dispatcher.calculate_event_time_difference(self.sent_events[0], self.sent_events[2])
-            self.assertGreater(transaction_time, 0)
-            self.assertAlmostEqual(self.time_till_start_of_response + self.time_till_end_of_response, transaction_time, delta=self.delta)
-
-
-    unittest.main()
-
     mon = VismoEventDispatcher('foo')
-    mon.send(tag='start-request', obj='ofdesire', status=1, operation='GET')
-    sleep(1)
-    mon.send(tag='start-response', obj='ofdesire', status=2, operation='GET')
-    sleep(1)
-    mon.send(tag='end-response', content_size=1000, obj='ofdesire', status=3, operation='GET')
 
+    def mon_send(operation, tag, tenant, user, container, obj):
+        mon.send(status='SUCCESS', tag=tag, operation=operation, tenant=tenant, user=user, container=container, obj=obj)
+
+    def send_put(tenant, user, container, obj):
+        mon_send('PUT', 'start-request', tenant, user, container, obj)
+        mon_send('PUT', 'start-response', tenant, user, container, obj)
+        mon_send('PUT', 'end-response', tenant, user, container, obj)
+
+    def send_get():
+        mon_send('GET', 'start-request', tenant, user, container, obj)
+        mon_send('GET', 'start-response', tenant, user, container, obj)
+        mon_send('GET', 'end-response', tenant, user, container, obj)
+
+    ctr = 0
+
+    for i in range(int(sys.argv[1])):
+        send_put('ntua', 'vassilis', 'test-container', 'foo')
+
+#{"status": "SUCCESS", "container": "events", "timestamp": 1361897370880, "object": "event-1361897364933", "transaction-duration": 5.4139999999999997, "content-size": 733, "originating-machine": "10.0.1.101", "user": "admin", "transaction-latency": 5.399, "transaction-throughput": 135.38973032877726, "originating-cluster": "test", "operation": "PUT", "type": "write", "id": "53638f2f-3c0a-4711-8ab1-01b836337f2c", "originating-service": "object_service", "tenant": "analytics"}
