2294abe38557a5541cdba34165606fdb89f97f92
vassilis <alexv@mail.ntua.gr>
2013-05-16 15:39:04 +0300

    updating cons/prod scripts

--- vismo-perf/src/main/scripts/consumer.sh
+++ vismo-perf/src/main/scripts/consumer.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
-CONF=${CONF:-config.properties}
-PORT=${PORT:-9992}
 prog=$(basename $0)
+CONF=${CONF:-config.properties}
 
-case $1 in
-	list*)
-		curl -v -X GET http://localhost:$PORT/handlers/ ;;
-	no*)
-		curl -v -X GET http://localhost:$PORT/handlers/"$2" ;;
-	new*)
-		curl -v -X PUT http://localhost:$PORT/handlers/"$2"/"$3" ;;
-	reset*)
-		curl -v -X POST http://localhost:$PORT/handlers/"$2" ;;
-	start*)
-		exec java -cp vismo-perf-*.jar gr.ntua.vision.monitoring.perf.Consumer "$CONF" $PORT >$2 & ;;
-	halt*)
-		curl -v -X POST http://localhost:$PORT/halt ;;
-	*)
-		echo "$prog: usage: $prog [start|list|no|new|reset|halt]" 2>&1 ;
-		exit 1 ;;
-esac
-
-exit 0
+java -cp vismo-perf-*.jar gr.ntua.vision.monitoring.perf.Consumer "$CONF" "$@"
 
--- vismo-perf/src/main/scripts/producer.sh
+++ vismo-perf/src/main/scripts/producer.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+prog=$(basename $2)
 CONF=${CONF:-config.properties}
 PORT=${PORT:-9991}
-prog=$(basename $0)
 
 case $1 in
 	send*)
 		# topic/rate/no-events/size
 		curl -v -X POST http://localhost:$PORT/events/$2/$3/$4/$5 ;;
 	start*)
-		exec java -cp vismo-perf-*.jar gr.ntua.vision.monitoring.perf.Producer "$CONF" $PORT & ;;
+		exec java -cp vismo-perf-*.jar gr.ntua.vision.monitoring.perf.Producer "$CONF" $PORT ;;
 	halt*)
 		curl -v -X POST http://localhost:$PORT/halt ;;
 	*)
