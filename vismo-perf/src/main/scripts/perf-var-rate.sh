a863fa8bef3869049cde5d0bbab722276134ccea
vassilis <alexv@mail.ntua.gr>
2013-05-21 11:53:58 +0300

    adding perf scripts

---
+++ vismo-perf/src/main/scripts/perf-var-rate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+export LANG=C
+source vismo-perf/src/main/scripts/prep-perf.sh
+
+function run_perf {
+	local no_events="$1"
+	local event_size="$2"
+	local max_rate="$3"
+	local results="$4"
+	local topic="perf-var-rate"
+	local tmp_out=perf.$$.tmp
+
+	rm -f "$results"
+	echo "# event-size, event-rate, no-events, latency-mean, throughput-mean" >"$results"
+
+	for rate in $(seq 100 50 "$max_rate")
+	do
+		java -cp vismo-perf-*.jar gr.ntua.vision.monitoring.perf.Consumer config.properties "$topic" "$event_size" "$no_events" >"$tmp_out" &
+		cons_pid=$!
+		sleep 3s
+
+		generate_events "$topic" "$rate" "$no_events" "$event_size"
+		wait $cons_pid
+
+		echo -n $event_size,$rate,$no_events, >>"$results"
+		"$STAT" "$tmp_out" | "$PARSE" >>"$results"
+		sleep 5s
+	done
+}
+
+#main 10000 1024 2000 perf-var-rate-results-$(date +'%F').csv
+main 1000 1024 500 perf-var-rate-results-$(date +'%F').csv
---
+++ vismo-perf/src/main/scripts/prep-perf.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -x
+
+export LANG=C
+
+VERSION.jar=1.1.5-SNAPSHOT
+TMP_DIR=/tmp/vismo-prep-perf.$$.tmp
+PROD=vismo-perf/src/main/scripts/producer.sh
+STAT=vismo-perf/src/main/scripts/csv-stat.py
+PARSE=vismo-perf/src/main/scripts/parse.awk
+VISMO_JAR=$(echo vismo-core/target/vismo-core-$VERSION.jar)
+PERF_JAR=$(echo vismo-perf/target/vismo-perf-$VERSION.jar)
+
+function start_producer {
+	"$PROD" start >/dev/null 2>&1 &
+	sleep 3s
+}
+
+function stop_producer {
+	"$PROD" halt 2>/dev/null &
+}
+
+function start_vismo {
+	nohup java -jar "$VISMO_JAR" config.properties start &
+}
+
+function stop_vismo {
+	java -jar "$VISMO_JAR" config.properties stop
+}
+
+function generate_events {
+	local topic="$1"
+	local rate="$2"
+	local no_events="$3"
+	local event_size="$4"
+
+	"$PROD" send "$topic" "$rate" "$no_events" "$event_size" 2>/dev/null
+}
+
+function warmup_vismo {
+	local topic=warmup-topic
+	local rate=1000.0
+	local no_events=2000
+	local event_size=1024
+	local timeout=1m
+
+	generate_events $topic $rate $no_events $event_size
+	sleep $timeout
+}
+
+function main {
+	start_vismo
+	start_producer
+	warmup_vismo
+
+	run_perf "$@"
+
+	stop_producer
+	stop_vismo
+	return 0
+}
