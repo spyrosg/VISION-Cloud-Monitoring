b6ab4951aec5987836a83c28083a48f30585174a
vassilis <alexv@mail.ntua.gr>
2013-05-22 12:56:16 +0300

    adding test to be implemented

--- vismo-core/src/test/java/integration/tests/HttpEventSourceTest.java
+++ vismo-core/src/test/java/integration/tests/HttpEventSourceTest.java
@@ -1,7 +1,15 @@
 package integration.tests;
 
+import org.junit.Test;
+
+
 /**
  * TODO: implement like {@link RulesResourceTest}.
  */
 public class HttpEventSourceTest {
+    /***/
+    @Test
+    public void shouldAcceptEventsThroughPUT() {
+        throw new AssertionError("to be implemented");
+    }
 }
