60419b284c9a28cee9ab1c9ee1f154f13f0ae19f
vassilis <alexv@mail.ntua.gr>
2013-06-05 14:48:10 +0300

    cleaning up HttpEventResource

--- vismo-core/src/main/java/gr/ntua/vision/monitoring/sources/HttpEventResource.java
+++ vismo-core/src/main/java/gr/ntua/vision/monitoring/sources/HttpEventResource.java
@@ -2,8 +2,6 @@ package gr.ntua.vision.monitoring.sources;
 
 import gr.ntua.vision.monitoring.events.EventFactory;
 import gr.ntua.vision.monitoring.events.MonitoringEvent;
-import gr.ntua.vision.monitoring.rules.VismoRulesEngine;
-import gr.ntua.vision.monitoring.sinks.EventSink;
 
 import java.net.InetAddress;
 import java.net.URI;
@@ -15,44 +13,28 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 
-
-
 /**
  * 
  */
 @Path("events")
 public class HttpEventResource implements EventSource {
-   
-	
-
-	
-
     /***/
-    private final EventFactory factory;
-    /***/
-    private EventSource source;
-   
+    private final EventFactory                   factory;
     /** the listeners lists. */
     private final ArrayList<EventSourceListener> listeners = new ArrayList<EventSourceListener>();
-    
-    private EventSourceListener listener;
-    
     /***/
-    private final ArrayList<EventSink> sinks = new ArrayList<EventSink>();
-    
-    
+    private EventSource                          source;
+
+
     /**
      * Constructor
      * 
      * @param factory
      */
-    public HttpEventResource (final EventFactory factory){
-    	this.factory = factory;
+    public HttpEventResource(final EventFactory factory) {
+        this.factory = factory;
     }
-   
-	
-    
-   
+
 
     /**
      * @see gr.ntua.vision.monitoring.sources.EventSource#add(gr.ntua.vision.monitoring.sources.EventSourceListener)
@@ -66,7 +48,7 @@ public class HttpEventResource implements EventSource {
 
     /**
      * @param body
-     * @return
+     * @return the response.
      */
     @PUT
     public Response putEvent(final String body) {
@@ -78,96 +60,85 @@ public class HttpEventResource implements EventSource {
         }
 
         return Response.created(URI.create("/")).build();
-        
     }
-    
+
+
     /**
      * Validate event's fields
      * 
-     * @param the event
-     * 
-     * @return
+     * @param ev
+     * @return the response.
      */
-    public Response eventValidation(MonitoringEvent ev){
-            validateIP(ev);
-            validateOriginatingService(ev);
-            validateEventTimestamp(ev);
-            validateEventTopic(ev);
-            
-            return Response.created(URI.create("/")).build();
+    private static Response eventValidation(final MonitoringEvent ev) {
+        validateIP(ev);
+        validateOriginatingService(ev);
+        validateEventTimestamp(ev);
+        validateEventTopic(ev);
+
+        return Response.created(URI.create("/")).build();
     }
-    
+
+
+    /**
+     * Validate Event's Timestamp
+     * 
+     * @param ev
+     * @return the response.
+     */
+    private static Response validateEventTimestamp(final MonitoringEvent ev) {
+        final Long timest = ev.timestamp();
+        if (timest == null)
+            return Response.status(400).entity("field timestamp required").build();
+
+        return Response.created(URI.create("/")).build();
+    }
+
+
+    /**
+     * Validate Event's topic
+     * 
+     * @param ev
+     * @return the response.
+     */
+    private static Response validateEventTopic(final MonitoringEvent ev) {
+        final String topic = ev.topic();
+        if (topic == null)
+            return Response.status(400).entity("field topic required").build();
+
+        return Response.created(URI.create("/")).build();
+    }
+
+
     /**
      * Validating Event IP
      * 
-     * @param the event
-     * 
-     * @return
+     * @param ev
+     * @return the response.
      */
-    public Response validateIP(MonitoringEvent ev){
-    	try {
+    private static Response validateIP(final MonitoringEvent ev) {
+        try {
             final InetAddress IP = ev.originatingIP();
             if (IP == null)
                 return Response.status(400).entity("No originating IP").build();
         } catch (final UnknownHostException e) {
             return Response.status(400).entity(e.getMessage()).build();
         }
-    	
-    	return Response.created(URI.create("/")).build();
+
+        return Response.created(URI.create("/")).build();
     }
-    
+
+
     /**
-     * Validate Event's Originating Service 
+     * Validate Event's Originating Service
      * 
-     * @param the event 
-     * 
-     * @return
+     * @param ev
+     * @return the response.
      */
-    public Response validateOriginatingService(MonitoringEvent ev){
-    	final String service = ev.originatingService();
+    private static Response validateOriginatingService(final MonitoringEvent ev) {
+        final String service = ev.originatingService();
         if (service == null)
             return Response.status(400).entity("field originating-service required").build();
-       
+
         return Response.created(URI.create("/")).build();
-    }
-    
-    /**
-     * Validate Event's Timestamp
-     * 
-     * @param the event 
-     * 
-     * @return
-     */
-    public Response validateEventTimestamp(MonitoringEvent ev){
-    	final Long timest = ev.timestamp();
-        if (timest == null)
-            return Response.status(400).entity("field timestamp required").build();
-    	
-        return Response.created(URI.create("/")).build();
-    }
-    
-    /**
-     * Validate Event's topic
-     * 
-     * @param the event 
-     * 
-     * @return
-     */
-    public Response validateEventTopic(MonitoringEvent ev){
-    	final String topic = ev.topic();
-        if (topic == null)
-            return Response.status(400).entity("field topic required").build();
-        
-        return Response.created(URI.create("/")).build();
-    }
-    
-    public Response receiveEvent(MonitoringEvent ev){
-    	listener.receive(ev);
-    	VismoRulesEngine engine = new VismoRulesEngine();
-    	engine.appendSinks(sinks);
-    	if (sinks != null){
-    		return Response.created(URI.create("/")).build();
-    	}
-    	return Response.status(400).entity("VismoRulesEngine didn't receive the event").build();
     }
 }
