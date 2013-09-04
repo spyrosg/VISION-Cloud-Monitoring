eaaa270001da5954ec30914bc289df81f3dcb7a3
vassilis <alexv@mail.ntua.gr>
2013-09-04 11:30:00 +0300

    adding some todos

--- vismo-core/src/main/java/gr/ntua/vision/monitoring/rules/ThresholdRule.java
+++ vismo-core/src/main/java/gr/ntua/vision/monitoring/rules/ThresholdRule.java
@@ -1,6 +1,9 @@
 package gr.ntua.vision.monitoring.rules;
 
 import static gr.ntua.vision.monitoring.rules.ThresholdRulesTraits.isApplicable;
+
+import java.util.List;
+
 import gr.ntua.vision.monitoring.events.MonitoringEvent;
 import gr.ntua.vision.monitoring.resources.ThresholdRuleBean;
 
@@ -15,7 +18,10 @@ public class ThresholdRule extends Rule {
     /** the log target. */
     private static final Logger            log = LoggerFactory.getLogger(Rule.class);
     /***/
-    private final String                   filterUnit;
+    // TODO: change filter unit to accept a list of containers.
+    // TODO: update period. 
+    // TODO: change #toString to show period.
+    private final List<String>                   filterUnits;
     /***/
     private final String                   operation;
     /***/
@@ -34,7 +40,7 @@ public class ThresholdRule extends Rule {
         super(engine);
         this.topic = bean.getTopic();
         this.operation = bean.getOperation();
-        this.filterUnit = bean.getFilterUnit();
+        this.filterUnits = bean.getFilterUnits();
         this.requirements = ThresholdRequirementList.from(bean.getRequirements());
     }
 
@@ -44,7 +50,7 @@ public class ThresholdRule extends Rule {
      */
     @Override
     public void performWith(final MonitoringEvent e) {
-        if (!isApplicable(e, filterUnit, operation, requirements))
+        if (!isApplicable(e, filterUnits, operation, requirements))
             return;
 
         log.debug("got applicable: {}", e);
@@ -63,7 +69,7 @@ public class ThresholdRule extends Rule {
      */
     @Override
     public String toString() {
-        return "#<ThresholdRule: " + topic + ", on " + filterUnit + " with " + requirements.size() + " requirements>";
+        return "#<ThresholdRule: " + topic + ", on " + filterUnits + " with " + requirements.size() + " requirements>";
     }
 
 
